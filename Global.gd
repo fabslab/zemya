4d1e5c3f283a10b5bebb29a55ca9b6a1d716909f
FabSlab <fabienbrooke@gmail.com>
2019-05-04 23:47:57 -0700

    run in godot 3.1.1

--- Global.gd
+++ Global.gd
@@ -3,9 +3,5 @@ extends Node
 func _ready():
 	hide_mouse()
 
-func _notification(event):
-	if event == MainLoop.NOTIFICATION_WM_FOCUS_IN:
-			hide_mouse()
-
 func hide_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
